2123c81f7e5812ea67c156838c66b26bdaac536f
Aleksei <trrskv@gmail.com>
2022-11-10 14:29:48 +0300

    add simple tests with allure reports

---
+++ test_negative.py
@@ -0,0 +1,14 @@
+def test_fail():
+    assert False
+
+def test_fail2():
+    assert False
+
+def test_fail3():
+    assert False
+
+def test_fail4():
+    assert False
+
+def test_fail5():
+    assert False
---
+++ test_positive.py
@@ -0,0 +1,20 @@
+def test_success():
+    assert True
+
+
+def test_success2():
+    assert True
+
+
+def test_success3():
+    assert True
+
+
+def test_success4():
+    assert True
+
+
+
+
+
+# allure serve allure-results/ - локально запустить алюр
